3fe3d4b20add98a35ab928073189f242e1863122
jamest11 <jameswtory@gmail.com>
2022-11-06 11:47:54 -0500

    Modify default layout page

--- src/App.js
+++ src/App.js
@@ -6,17 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Welcome to Full Stack Development I</h2>
+        <h3>React JS Programming Week 9 Lab Exercise</h3>
+        <h4>101049351</h4>
+        <h4>James Tory</h4>
+        <h5>George Brown College, Toronto</h5>
       </header>
     </div>
   );
